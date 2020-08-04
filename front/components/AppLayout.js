5de6ba5a5b789345655e9ab1bec36d34bbee1a1c
qnrjs42 <qnrjs42@naver.com>
2020-08-04 12:33:30 +0900

    레이아웃2 개행처리

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -35,15 +35,18 @@ const AppLayout = () => {
           paddingTop: "80px",
         }}
       >
-        <Row justify="center">
-          <h1>링크는 CUT으로 간단하게, 그것뿐이니까</h1>
-          <p>
-            고객을 놓치지 않고 매출로 연결하며, 떠나간 고객도 메시지를 보내
-            돌아오게 합니다. 꼭 필요한 팀 메신저도 무료로 제공됩니다.
-          </p>
-          <br />
-
-          <Row justify="center" style={{ width: "80vw", paddingTop: "3vh" }}>
+        <div style={{ padding: '10px'}}>
+          <Row justify="center">
+            <h1>링크는 CUT으로 간단하게, 그것뿐이니까</h1>
+          </Row>
+          <Row justify="center">
+            <p>
+              고객을 놓치지 않고 매출로 연결하며, 떠나간 고객도 메시지를 보내
+              돌아오게 합니다. 꼭 필요한 팀 메신저도 무료로 제공됩니다.
+            </p>
+            <br />
+          </Row>
+          <Row justify="center" style={{ paddingTop: "3vh" }}>
             <Col style={{ marginRight: "3vw" }}>
               <Button type="dashed" icon={<WechatFilled />} size="large">
                 채팅 상담
@@ -62,7 +65,7 @@ const AppLayout = () => {
               </Button>
             </Col>
           </Row>
-        </Row>
+        </div>
       </Layout>
       <br />
 
