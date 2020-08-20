6c215e2be212d7c051cda5dca0ecd2be6c227108
qnrjs42 <qnrjs42@naver.com>
2020-08-21 00:33:03 +0900

    Drawer 수정, 검색 인풋, 프로필 버튼 생성

--- front/components/UserLayout/SiderSection/DrawerSection.js
+++ front/components/UserLayout/SiderSection/DrawerSection.js
@@ -7,6 +7,8 @@ import {
   UnorderedListOutlined,
 } from "@ant-design/icons";
 
+const { Header } = Layout;
+
 
 const DrawerSection = () => {
 
@@ -22,64 +24,110 @@ const DrawerSection = () => {
 
     return (
       <>
-        <div className="mobile-sider-on-off">
-          <UnorderedListOutlined onClick={showDrawer} />
+        <div>
+          <div className="mobile-sider-on-off">
+            <UnorderedListOutlined onClick={showDrawer} />
 
-          <Drawer
-            title="Link_Project"
-            placement="left"
-            closable={false}
-            onClose={onClose}
-            visible={Visible}
-            getContainer={false}
-            style={{ position: "absolute" }}
-          >
-            <Menu>
-              <span style={{ padding: "10px 0 10px 0" }}>DASHBOARD</span>
-              <Menu.Item key="12" icon={<PieChartOutlined />} onClick={onClose}>
-                <span>관리페이지</span>
-              </Menu.Item>
-              <Menu.Item key="13" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>전체 링크 관리</span>
-              </Menu.Item>
-              <Menu.Divider />
+            <Drawer
+              title="Link_Project"
+              placement="left"
+              closable={false}
+              onClose={onClose}
+              visible={Visible}
+              getContainer={false}
+              style={{ position: "absolute" }}
+            >
+              <Menu>
+                <span style={{ padding: "10px 0 10px 0" }}>DASHBOARD</span>
+                <Menu.Item
+                  key="12"
+                  icon={<PieChartOutlined />}
+                  onClick={onClose}
+                >
+                  <span>관리페이지</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="13"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>전체 링크 관리</span>
+                </Menu.Item>
+                <Menu.Divider />
 
-              <span>MANAGEMENT</span>
-              <Menu.Item key="14" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>링크 보관함</span>
-              </Menu.Item>
-              <Menu.Item key="15" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>설정기간 만료</span>
-              </Menu.Item>
-              <Menu.Item key="16" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>링크 패키지</span>
-              </Menu.Item>
-              <Menu.Divider />
+                <span>MANAGEMENT</span>
+                <Menu.Item
+                  key="14"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>링크 보관함</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="15"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>설정기간 만료</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="16"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>링크 패키지</span>
+                </Menu.Item>
+                <Menu.Divider />
 
-              <span>TOOLS</span>
-              <Menu.Item key="17" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>빠른 단축URL 생성</span>
-              </Menu.Item>
-              <Menu.Item key="18" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>북마클릿</span>
-              </Menu.Item>
-              <Menu.Item key="19" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>개발용 API</span>
-              </Menu.Item>
-              <Menu.Item key="20" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>전체 페이지 스크립트</span>
-              </Menu.Item>
-              <Menu.Divider />
+                <span>TOOLS</span>
+                <Menu.Item
+                  key="17"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>빠른 단축URL 생성</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="18"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>북마클릿</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="19"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>개발용 API</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="20"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>전체 페이지 스크립트</span>
+                </Menu.Item>
+                <Menu.Divider />
 
-              <span>PRIVACY</span>
-              <Menu.Item key="21" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>프로필 설정</span>
-              </Menu.Item>
-              <Menu.Item key="22" icon={<DesktopOutlined />} onClick={onClose}>
-                <span>결제 정보</span>
-              </Menu.Item>
-            </Menu>
-          </Drawer>
+                <span>PRIVACY</span>
+                <Menu.Item
+                  key="21"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>프로필 설정</span>
+                </Menu.Item>
+                <Menu.Item
+                  key="22"
+                  icon={<DesktopOutlined />}
+                  onClick={onClose}
+                >
+                  <span>결제 정보</span>
+                </Menu.Item>
+              </Menu>
+            </Drawer>
+          </div>
         </div>
       </>
     );
--- front/components/UserLayout/UserLayout.js
+++ front/components/UserLayout/UserLayout.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Layout, Menu, Breadcrumb, Row, Col, Input } from "antd";
+import { Layout, Menu, Breadcrumb, Row, Col, Input, Avatar, Dropdown } from "antd";
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -8,12 +8,28 @@ import {
   UserOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  
+  MessageOutlined,
+  DownOutlined,
 } from "@ant-design/icons";
 
 import SiderMenu from './SiderMenu';
 
+
 const { Header, Content, Footer, Sider } = Layout;
+const { Search } = Input;
+
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a>프로필 설정</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="1">
+      <a>로그아웃</a>
+    </Menu.Item>
+  </Menu>
+);
 
 const UserLayout = () => {
     const [Cllapsed, setCllapsed] = useState(false);
@@ -30,8 +46,37 @@ const UserLayout = () => {
           <SiderMenu />
 
           <Layout className="site-layout">
-            
-            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Header style={{ backgroundColor: "white" }}>
+              <Row gutter={[16, 16]}>
+                <Col span={16}>
+                  <Search
+                    placeholder="input search text"
+                    onSearch={(value) => console.log(value)}
+                    size="large"
+                    enterButton
+                    style={{
+                      paddingTop: "12px",
+                      maxWidth: "60%",
+                      minWidth: "40vw",
+                    }}
+                  />
+                </Col>
+                <Col span={8} style={{ textAlign: "right" }}>
+                  <Dropdown overlay={menu} trigger={["click"]} placement="bottomCenter">
+                    <a
+                      className="ant-dropdown-link"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <Avatar
+                        size="large"
+                        icon={<UserOutlined />}
+                        style={{ backgroundColor: "#3880FF" }}
+                      />
+                    </a>
+                  </Dropdown>
+                </Col>
+              </Row>
+            </Header>
 
             <Content style={{ margin: "0 16px" }}>
               <Breadcrumb style={{ margin: "16px 0" }}>
@@ -41,9 +86,7 @@ const UserLayout = () => {
               <div
                 className="site-layout-background"
                 style={{ padding: 24, minHeight: 360 }}
-              >
-                Bill is a cat.
-              </div>
+              ></div>
             </Content>
             <Row className="footer-terms" justify="center">
               We Are ©2020 Created by sistinafibel And qnrjs42
