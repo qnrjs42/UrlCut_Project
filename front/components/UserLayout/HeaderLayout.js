8a049c4ee8d228f5452a4e1abe55b7e951660b81
qnrjs42 <qnrjs42@naver.com>
2020-08-22 18:47:21 +0900

    HeaderLayout 분리, HeaderLayout 모바일 그리드로 수정

---
+++ front/components/UserLayout/HeaderLayout.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Row,
+  Col,
+  Input,
+  Avatar,
+  Dropdown,
+} from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  MessageOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+
+import DrawerSection from "./SiderSection/DrawerSection";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { Search } = Input;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a>프로필 설정</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="1">
+      <a>로그아웃</a>
+    </Menu.Item>
+  </Menu>
+);
+
+const HeaderLayout = () => {
+  return (
+    <Header style={{ backgroundColor: "white" , padding: '0 20px'}}>
+      <Row>
+        <DrawerSection />
+
+        <Col xs={16}>
+          <Search
+            placeholder="input search text"
+            onSearch={(value) => console.log(value)}
+            size="large"
+            enterButton
+            style={{
+              paddingTop: "12px",
+              maxWidth: "60%",
+              minWidth: "40vw",
+            }}
+          />
+        </Col>
+        <Col flex={2}> 
+        <Row justify="end">          
+          <Dropdown overlay={menu} trigger={["click"]} placement="bottomCenter">
+            <a
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              <Avatar
+                size="large"
+                icon={<UserOutlined />}
+                style={{ backgroundColor: "#3880FF" }}
+              />
+            </a>
+          </Dropdown>
+          </Row>
+        </Col>
+      </Row>
+    </Header>
+  );
+};
+
+export default HeaderLayout;
--- front/components/UserLayout/UserLayout.js
+++ front/components/UserLayout/UserLayout.js
@@ -12,82 +12,23 @@ import {
   DownOutlined,
 } from "@ant-design/icons";
 
-import SiderMenu from './SiderMenu';
+import SiderMenu from './SiderSection/SiderMenu';
+import HeaderLayout from './HeaderLayout';
+import MainManageLayout from './Dashboard/MainManageLayout';
 
 
-const { Header, Content, Footer, Sider } = Layout;
-const { Search } = Input;
-
-
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      <a>프로필 설정</a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="1">
-      <a>로그아웃</a>
-    </Menu.Item>
-  </Menu>
-);
+const { Footer } = Layout;
 
 const UserLayout = () => {
-    const [Cllapsed, setCllapsed] = useState(false);
-
-
-    const toggle = () => {
-      setCllapsed(!Cllapsed);
-    };
-
-
     return (
       <div className="user">
         <Layout style={{ minHeight: "100vh" }}>
           <SiderMenu />
 
           <Layout className="site-layout">
-            <Header style={{ backgroundColor: "white" }}>
-              <Row gutter={[16, 16]}>
-                <Col span={16}>
-                  <Search
-                    placeholder="input search text"
-                    onSearch={(value) => console.log(value)}
-                    size="large"
-                    enterButton
-                    style={{
-                      paddingTop: "12px",
-                      maxWidth: "60%",
-                      minWidth: "40vw",
-                    }}
-                  />
-                </Col>
-                <Col span={8} style={{ textAlign: "right" }}>
-                  <Dropdown overlay={menu} trigger={["click"]} placement="bottomCenter">
-                    <a
-                      className="ant-dropdown-link"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <Avatar
-                        size="large"
-                        icon={<UserOutlined />}
-                        style={{ backgroundColor: "#3880FF" }}
-                      />
-                    </a>
-                  </Dropdown>
-                </Col>
-              </Row>
-            </Header>
+            <HeaderLayout />
+            <MainManageLayout />
 
-            <Content style={{ margin: "0 16px" }}>
-              <Breadcrumb style={{ margin: "16px 0" }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              <div
-                className="site-layout-background"
-                style={{ padding: 24, minHeight: 360 }}
-              ></div>
-            </Content>
             <Row className="footer-terms" justify="center">
               We Are ©2020 Created by sistinafibel And qnrjs42
             </Row>
