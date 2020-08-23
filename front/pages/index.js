5133b8dfca2e3f1e6d8a6fbeeaa7ca63d83d9281
qnrjs42 <qnrjs42@naver.com>
2020-08-23 11:37:25 +0900

    no message

--- front/pages/index.js
+++ front/pages/index.js
@@ -10,31 +10,28 @@ const Home = () => {
   const [value, setValue] = useState(() => {
     if (typeof window !== "undefined") {
       const stickyValue = window.localStorage.getItem("me");
-      console.log('value: ', value);
-      console.log("stickyValue: ", typeof stickyValue);
-      console.log("me: ", me);
+      
       return stickyValue !== null ? JSON.parse(stickyValue) : null;
     }
   });
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // window.localStorage.setItem("me", JSON.stringify(value));
-      window.localStorage.setItem("me", value);
+      window.localStorage.setItem("me", JSON.stringify(value));
     }
 
-    // if(value) { // me 정보가 있는 상태에서 루트로 이동 시 /user로 리다이렉션
-    //   if (Router.pathname === "/") {
-    //     Router.push("/user");
-    //   }
-    // }
-
-    if (me) {
-      // me 정보가 있는 상태에서 루트로 이동 시 /user로 리다이렉션
+    if(value) { // me 정보가 있는 상태에서 루트로 이동 시 /user로 리다이렉션
       if (Router.pathname === "/") {
         Router.push("/user");
       }
     }
+
+    // if (me) {
+    //   // me 정보가 있는 상태에서 루트로 이동 시 /user로 리다이렉션
+    //   if (Router.pathname === "/") {
+    //     Router.push("/user");
+    //   }
+    // }
   }, [value]);
   return (
     <>
