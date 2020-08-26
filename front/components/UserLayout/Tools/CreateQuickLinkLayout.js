d29118a79cb6885a9249ee3019edb0fa0f9273b6
qnrjs42 <qnrjs42@naver.com>
2020-08-26 20:20:56 +0900

    Tools 컴포넌트 - 레이아웃 구성

--- front/components/UserLayout/Tools/CreateQuickLinkLayout.js
+++ front/components/UserLayout/Tools/CreateQuickLinkLayout.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Layout, Row, Col, Button, Card, Progress, Table } from "antd";
+import { LinkOutlined, InfoCircleOutlined } from "@ant-design/icons";
+
+const { Content } = Layout;
 
 const CreateQuickLinkLayout = () => {
-    return (
-        <div>
-            CreateQuickLinkLayout
-        </div>
-    )
-}
+  return (
+    <Content style={{ margin: "20px 16px" }}>
+      <Row gutter={[16, 16]} justify="center" style={{ paddingTop: "20px" }}>
+        <Col
+          span={16}
+          xs={{ span: 24, order: 1 }}
+          sm={{ span: 24, order: 1 }}
+          lg={{ span: 16 }}
+        >
+          <Card style={{ height: 'auto' }}>
+            <h3>빠른 단축 URL</h3>
+            <p>
+              이 방법을 사용하면 VOLA 서비스에 방문하지 않고도 단축URL을 빠르게
+              생성할 수 있습니다. 단, 해당 브라우저를 통해 VOLA사이트에
+              마지막으로 로그인한 계정과 연동됩니다. (쿠키데이터 활용됩니다.)
+              사용방법은 단축하고자 하는 웹페이지로 이동한 후 브라우저 주소창에
+              표기된 URL앞에 https://link_project/q/?u= 를 추가하면 회원님의
+              대시보드에 단축URL이 생성됩니다.
+            </p>
+            <div
+              className="code-box"
+              style={{ backgroundColor: "#EAEAEA", padding: 10 }}
+            >
+              https://link_project/q/?u=URL_OF_SITE
+            </div>
 
-export default CreateQuickLinkLayout
+            <br />
+            <br />
+
+            <p>예제들</p>
+            <div
+              className="code-box"
+              style={{ backgroundColor: "#EAEAEA", padding: 10 }}
+            >
+              https://link_project/q/?u=https://www.google.com
+              <br />
+              https://link_project/q/?u=naver.com
+              <br />
+              https://link_project/q/?u=http://www.apple.com/iphone-7/
+              <br />
+            </div>
+
+            <br />
+            <br />
+            <p>메모</p>
+            <p>
+              이 방법은 해당 웹페이지에서 다른 링크로 이동 되지 않습니다. 단순히
+              마지막에 로그인한 VOLA계정에 변환된 단축URL이 회원 관리페이지에
+              저장됩니다.
+            </p>
+          </Card>
+        </Col>
+        <Col
+          span={8}
+          xs={{ span: 24, order: 1 }}
+          sm={{ span: 24, order: 2 }}
+          lg={{ span: 8 }}
+        >
+          <Card style={{ height: "auto" }}>
+            <h3>
+              <InfoCircleOutlined /> 정보
+            </h3>
+            <p>
+              API키가 외부에 노출이 된다면, 악용될 소지가 있으니, 회원님의
+              계정은 물론 API키 보안에 주의하시길 바랍니다.
+            </p>
+          </Card>
+        </Col>
+      </Row>
+    </Content>
+  );
+};
+
+export default CreateQuickLinkLayout;
--- front/components/UserLayout/Tools/FullPageScriptLayout.js
+++ front/components/UserLayout/Tools/FullPageScriptLayout.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Layout, Row, Col, Button, Card, Progress, Table } from "antd";
+import { LinkOutlined, InfoCircleOutlined } from "@ant-design/icons";
+
+const { Content } = Layout;
+
+const script1 = `
+<script type="text/javascript">var key = "RyazHlyM5103";</script>
+<script
+  type="text/javascript"
+  src="https://vo.la/jShortener">
+  </script>
+`;
+
+const script2 = `
+<script type="text/javascript">
+var key = "RyazHlyM5103";
+var selector = ".mylink, .content > a, .comments a";
+</script>
+<script type="text/javascript" src="https://vo.la/jShortener"></script>
+`;
+
+const script3 = `
+<script type="text/javascript">
+var key = "RyazHlyM5103";
+var exclude = ["google.com","naver.com"];
+</script>
+<script type="text/javascript" src="https://vo.la/jShortener"></script>
+`;
 
 const FullPageScriptLayout = () => {
-    return (
-        <div>
-            FullPageScriptLayout
-        </div>
-    )
-}
+  return (
+    <Content style={{ margin: "20px 16px" }}>
+      <Row gutter={[16, 16]} justify="center" style={{ paddingTop: "20px" }}>
+        <Col
+          span={16}
+          xs={{ span: 24, order: 1 }}
+          sm={{ span: 24, order: 1 }}
+          lg={{ span: 16 }}
+        >
+          <Card style={{ height: "auto" }}>
+            <h3>전체 페이지 스크립트</h3>
+            <p>
+              이 스크립트를 사용하면 웹 사이트의 URL을 모두 짧게 줄일 수 있으며,
+              특정 선택자에서 URL을 타겟팅하려는 경우 선택자를 맞춤 설정할 수
+              있습니다. 아래 소스코드는 회원님의 API키가 적용된 소스로 바로
+              복사해 적용하시면 됩니다.
+            </p>
+            <div
+              className="code-box"
+              style={{ backgroundColor: "#EAEAEA", padding: 10 }}
+            >
+              <pre>{script1}</pre>
+            </div>
+            <br />
+            <br />
 
-export default FullPageScriptLayout
+            <h3>맞춤식 셀렉트 선택</h3>
+            <p>
+              기본적으로 이 스크립트는 페이지의 모든 URL을 짧게 만듭니다. 특정
+              URL을 대상으로 하려면 선택 도구 매개 변수를 추가할 수 있습니다.
+              아래에서 스크립트가 mylink라는 클래스나. contents컨테이너에 있는
+              모든 직접 링크 또는 주석 컨테이너의 모든 링크를 포함하는 URL을
+              단축하는 예만 볼 수 있습니다.
+            </p>
+            <div
+              className="code-box"
+              style={{ backgroundColor: "#EAEAEA", padding: 10 }}
+            >
+              <pre>{script2}</pre>
+            </div>
+            <br />
+            <br />
+
+            <h3>도메인 이름 제외</h3>
+            <p>
+              원하는 경우 도메인 이름을 제외할 수 있습니다. 도메인 이름을
+              제외하는 제외 매개 변수를 추가할 수 있습니다. 아래의 예는
+              google.com 또는 naver.com에서 URL을 제외한 모든 URL을 줄인
+              것입니다.
+            </p>
+            <div
+              className="code-box"
+              style={{ backgroundColor: "#EAEAEA", padding: 10 }}
+            >
+              <pre>{script3}</pre>
+            </div>
+          </Card>
+        </Col>
+        <Col
+          span={8}
+          xs={{ span: 24, order: 1 }}
+          sm={{ span: 24, order: 2 }}
+          lg={{ span: 8 }}
+        >
+          <Card style={{ height: "auto" }}>
+            <h3>
+              <InfoCircleOutlined /> 정보
+            </h3>
+            <p>
+              API키가 외부에 노출이 된다면, 악용될 소지가 있으니, 회원님의
+              계정은 물론 API키 보안에 주의하시길 바랍니다.
+            </p>
+          </Card>
+        </Col>
+      </Row>
+    </Content>
+  );
+};
+
+export default FullPageScriptLayout;
