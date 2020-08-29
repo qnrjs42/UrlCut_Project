4c45d62d80bcfa94347e19e4e057d28031f349e8
qnrjs42 <qnrjs42@naver.com>
2020-08-29 12:14:51 +0900

    메인 - 백그라운드 이미지 버그 수정

--- front/components/MainLayout/MainLayout.js
+++ front/components/MainLayout/MainLayout.js
@@ -11,141 +11,138 @@ const FormWrapper = styled(Form)`
 `;
 
 const Main = () => {
-    const dispatch = useDispatch();
-    const urlInfo = useSelector((state) => state.url);
-    const { me } = useSelector((state) => state.user);
-    
-    const [Url, setUrl] = useState("");
-    const [ChangeUrl, setChangeUrl] = useState("");
-    const [UrlLoading, setUrlLoading] = useState(false);
-    const [Copyed, setCopyed] = useState(false);
+  const dispatch = useDispatch();
+  const urlInfo = useSelector((state) => state.url);
+  const { me } = useSelector((state) => state.user);
 
-    const [MainLayout, setMainLayout] = useState("main-layout-before");
+  const [Url, setUrl] = useState("");
+  const [ChangeUrl, setChangeUrl] = useState("");
+  const [UrlLoading, setUrlLoading] = useState(false);
+  const [Copyed, setCopyed] = useState(false);
 
-    const onChangeUrl = useCallback((e) => {
-      setChangeUrl(e.target.value);
-    }, []);
+  const [MainLayout, setMainLayout] = useState("main-layout-before");
 
-    const onSubmitForm = useCallback(() => {
-      setUrlLoading(true);
-      setUrl(ChangeUrl);
-      setCopyed(true);
-      setMainLayout("main-layout-after");
+  const onChangeUrl = useCallback((e) => {
+    setChangeUrl(e.target.value);
+  }, []);
 
-      dispatch(urlCutRequestAction());
+  const onSubmitForm = useCallback(() => {
+    setUrlLoading(true);
+    setUrl(ChangeUrl);
+    setCopyed(true);
+    setMainLayout("main-layout-after");
 
-      setUrlLoading(false);
-    }, [ChangeUrl]);
+    dispatch(urlCutRequestAction());
 
-    return (
-        // <Layout className={`layout ${MainLayout}`}>
-        <Layout className="layout main-layout-before">
-          {/* <div className="main-bg-image"></div> */}
-          {/* <div className="main-bg-text"> */}
-          <div>
-            <FormWrapper onFinish={onSubmitForm} style={{ paddingTop: 150 }}>
-              <Row justify="center">
-                <h1>링크는 간단하게 CUT</h1>
-              </Row>
-              <Row justify="center">
-                {/* xs: <576, sm: >=576 md: >=768, lg: >=992, xl: >=1200 */}
-                <Col
-                  xs={32}
-                  sm={15}
-                  md={13}
-                  lg={10}
-                  xl={9}
-                  style={{ textAlign: "center" }}
-                >
-                  <Input
-                    size="large"
-                    name="url"
-                    placeholder="URL Paste"
-                    value={ChangeUrl}
-                    onChange={onChangeUrl}
-                    required
-                    autoComplete="off"
-                    style={{
-                      borderRadius: "0px",
-                      padding: "0 0 0 11px",
-                      height: "55px",
-                    }}
-                    prefix={<LinkOutlined />}
-                    suffix={
+    setUrlLoading(false);
+  }, [ChangeUrl]);
+
+  return (
+    <Layout className="layout main-layout">
+      <div>
+        <FormWrapper onFinish={onSubmitForm} style={{ paddingTop: 150 }}>
+          <Row justify="center">
+            <h1>링크는 간단하게 CUT</h1>
+          </Row>
+          <Row justify="center">
+            {/* xs: <576, sm: >=576 md: >=768, lg: >=992, xl: >=1200 */}
+            <Col
+              xs={32}
+              sm={15}
+              md={13}
+              lg={10}
+              xl={9}
+              style={{ textAlign: "center" }}
+            >
+              <Input
+                size="large"
+                name="url"
+                placeholder="URL Paste"
+                value={ChangeUrl}
+                onChange={onChangeUrl}
+                required
+                autoComplete="off"
+                style={{
+                  borderRadius: "0px",
+                  padding: "0 0 0 11px",
+                  height: "55px",
+                }}
+                prefix={<LinkOutlined />}
+                suffix={
+                  <Button
+                    type="primary"
+                    style={{ height: "55px", borderRadius: "0px" }}
+                    htmlType="submit"
+                    loading={UrlLoading}
+                  >
+                    CUT
+                  </Button>
+                }
+              />
+            </Col>
+          </Row>
+
+          {Copyed ? (
+            <Row justify="center" style={{ marginTop: "4vh" }}>
+              <Col xs={32} sm={14} md={12} lg={10} xl={7}>
+                <div className="site-card-border-less-wrapper">
+                  <Card
+                    title={Url}
+                    bordered={false}
+                    style={{ borderRadius: "0px" }}
+                  >
+                    <div>
+                      <p
+                        style={{
+                          display: "inline-block",
+                          marginRight: "5px",
+                        }}
+                      >
+                        단축링크 : {urlInfo.url}
+                      </p>
+                      <p style={{ display: "inline-block" }}></p>
+                    </div>
+                    <Divider dashed />
+                    <div style={{ textAlign: "center" }}>
                       <Button
                         type="primary"
-                        style={{ height: "55px", borderRadius: "0px" }}
-                        htmlType="submit"
-                        loading={UrlLoading}
+                        size="large"
+                        style={{ height: "45px", borderRadius: "0px" }}
                       >
-                        CUT
+                        로그인
                       </Button>
-                    }
-                  />
-                </Col>
-              </Row>
-
-              {Copyed ? (
-                <Row justify="center" style={{ marginTop: "4vh" }}>
-                  <Col xs={32} sm={14} md={12} lg={10} xl={7}>
-                    <div className="site-card-border-less-wrapper">
-                      <Card
-                        title={Url}
-                        bordered={false}
-                        style={{ borderRadius: "0px" }}
-                      >
-                        <div>
-                          <p
-                            style={{
-                              display: "inline-block",
-                              marginRight: "5px",
-                            }}
-                          >
-                            단축링크 : {urlInfo.url}
-                          </p>
-                          <p style={{ display: "inline-block" }}></p>
-                        </div>
-                        <Divider dashed />
-                        <div style={{ textAlign: "center" }}>
-                          <Button
-                            type="primary"
-                            size="large"
-                            style={{ height: "45px", borderRadius: "0px" }}
-                          >
-                            로그인
-                          </Button>
-                        </div>
-                      </Card>
                     </div>
-                  </Col>
-                </Row>
-              ) : (
-                <div></div>
-              )}
-
-              <Row justify="center" style={{ marginTop: "4vh" }}>
-                <p>URL이 너무 길다구요? 'CUT'하면 짧아지는 마술~</p>
-              </Row>
-            </FormWrapper>
-
-            <Row justify="center" style={{ marginTop: "6vh" }}>
-              <div className="main-image-container">
-                <div className="main-image-list">
-                  <div className="main-image-item main-image-margin">
-                    <img src="../static/IU1.jpg" className="main-image-content" />
-                  </div>
-                  <div className="main-image-item main-image-margin">
-                    <img src="../static/IU2.jpg" className="main-image-content" />
-                  </div>
-                  <div className="main-image-item">
-                    <img src="../static/IU3.jpg" className="main-image-content" />
-                  </div>
+                  </Card>
                 </div>
-              </div>
+              </Col>
             </Row>
-          </div>
-        </Layout>
-    );
-}
+          ) : (
+            <div></div>
+          )}
 
-export default Main
+          <Row justify="center" style={{ marginTop: "4vh" }}>
+            <p>URL이 너무 길다구요? 'CUT'하면 짧아지는 마술~</p>
+          </Row>
+        </FormWrapper>
+
+        <Row justify="center" style={{ marginTop: "6vh" }}>
+          <div className="main-image-container">
+            <div className="main-image-list">
+              <div className="main-image-item main-image-margin">
+                <img src="../static/IU1.jpg" className="main-image-content" />
+              </div>
+              <div className="main-image-item main-image-margin">
+                <img src="../static/IU2.jpg" className="main-image-content" />
+              </div>
+              <div className="main-image-item">
+                <img src="../static/IU3.jpg" className="main-image-content" />
+              </div>
+            </div>
+          </div>
+        </Row>
+      </div>
+    </Layout>
+  );
+};
+
+export default Main;
