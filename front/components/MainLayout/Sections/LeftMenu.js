efd8cac8264881317c2d44115b7a372fe6000db3
qnrjs42 <qnrjs42@naver.com>
2020-09-02 23:09:29 +0900

    메뉴 a 태그 이동 대신 Link 태그로 이동

--- front/components/MainLayout/Sections/LeftMenu.js
+++ front/components/MainLayout/Sections/LeftMenu.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React from "react";
+import { Menu } from "antd";
 import Link from "next/link";
 
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
       <Menu.Item key="home">
-        <a href="/">Home</a>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
       </Menu.Item>
       <Menu.Item key="2">
-        <a href="/">URL 안전검사</a>
+        <Link href="/">
+          <a>URL 안전검사</a>
+        </Link>
       </Menu.Item>
       <Menu.Item key="3">
-        <a href="/">부가 서비스</a>
+        <Link href="/">
+          <a>부가 서비스</a>
+        </Link>
       </Menu.Item>
     </Menu>
   );
 }
 
-export default LeftMenu
+export default LeftMenu;
